0d3cee046144d8e4330bc3ce667698fa748e7422
Peter Young <young40@gmail.com>
2019-07-21 01:08:08 +0800

    Initial Commit

---
+++ AnotherSwiftUIWeather/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  AnotherSwiftUIWeather
+//
+//  Created by 杨世玲 on 2019/7/21.
+//  Copyright © 2019 杨世玲. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
